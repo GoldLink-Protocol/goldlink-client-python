373b7cbc7e8c8dfebe521c8cac5bda559ea1b4d5
Sam Weinberg <samweinberg@sams-mbp.lan>
2024-03-06 12:58:42 -0800

    final python client methods

--- goldlink/modules/strategies/gmx_frf/gmx_frf_writer.py
+++ goldlink/modules/strategies/gmx_frf/gmx_frf_writer.py
@@ -21,6 +21,10 @@ class GmxFrfWriter(ContractHandler, TransactionHandler):
 
         self.default_address = default_address
 
+    # -----------------------------------------------------------
+    # Core Transactions
+    # -----------------------------------------------------------
+
     def create_increase_order(
             self,
             strategy_account,
@@ -51,6 +55,40 @@ class GmxFrfWriter(ContractHandler, TransactionHandler):
             method=self.get_gmxfrf_strategy_account(strategy_account).functions.executeCreateIncreaseOrder(
                 market=market,
                 collateralAmount=amount
+            ),
+            options=send_options,
+        )
+    
+    def create_decrease_order(
+            self,
+            strategy_account,
+            market,
+            size_delta_usd,
+            send_options=None
+    ):
+        '''
+        Create a decrease order for a position.
+
+        :param strategy_account: required
+        :type strategy_account: address
+
+        :param market: required
+        :type market: address
+
+        :param size_delta_usd: required
+        :type size_delta_usd: integer
+
+        :param send_options: optional
+        :type send_options: sendOptions
+
+        :returns: transactionHash
+
+        :raise: TransactionReverted
+        ''' 
+        return self.send_transaction(
+            method=self.get_gmxfrf_strategy_account(strategy_account).functions.executeCreateDecreaseOrder(
+                market=market,
+                sizeDeltaUsd=size_delta_usd
             ),
             options=send_options,
         )
@@ -156,4 +194,183 @@ class GmxFrfWriter(ContractHandler, TransactionHandler):
             ),
             options=send_options,
         )
+    
+    # -----------------------------------------------------------
+    # Liquidation Transactions
+    # -----------------------------------------------------------
 
+    def liquidate_assets(
+            self,
+            strategy_account,
+            asset,
+            amount,
+            callback,
+            receiver,
+            send_options=None
+    ):
+        '''
+        Liquidate an amount of an asset for an underwater strategy account.
+
+        :param strategy_account: required
+        :type strategy_account: address
+
+        :param asset: required
+        :type asset: address
+
+        :param amount: required
+        :type amount: integer
+
+        :param callback: required
+        :type callback: address
+
+        :param receiver: required
+        :type receiver: address
+
+        :param send_options: optional
+        :type send_options: sendOptions
+
+        :returns: None
+
+        :raise: TransactionReverted
+        ''' 
+        return self.send_transaction(
+            method=self.get_gmxfrf_strategy_account(strategy_account).functions.executeLiquidateAssets(
+                asset=asset,
+                amount=amount,
+                callback=callback,
+                receiever=receiver
+            ),
+            options=send_options,
+        )
+
+    def liquidate_position(
+            self,
+            strategy_account,
+            market,
+            size_delta_usd,
+            send_options=None
+    ):
+        '''
+        Liquidate an amount of a position for an underwater strategy account.
+
+        :param strategy_account: required
+        :type strategy_account: address
+
+        :param market: required
+        :type market: address
+
+        :param size_delta_usd: required
+        :type size_delta_usd: integer
+
+        :param send_options: optional
+        :type send_options: sendOptions
+
+        :returns: None
+
+        :raise: TransactionReverted
+        ''' 
+        return self.send_transaction(
+            method=self.get_gmxfrf_strategy_account(strategy_account).functions.executeLiquidatePosition(
+                market=market,
+                sizeDeltaUsd=size_delta_usd
+            ),
+            options=send_options,
+        )
+    
+    def rebalance_position(
+            self,
+            strategy_account,
+            market,
+            send_options=None
+    ):
+        '''
+        Rebalance a position for a strategy account.
+
+        :param strategy_account: required
+        :type strategy_account: address
+
+        :param market: required
+        :type market: address
+
+        :param send_options: optional
+        :type send_options: sendOptions
+
+        :returns: None
+
+        :raise: TransactionReverted
+        ''' 
+        return self.send_transaction(
+            method=self.get_gmxfrf_strategy_account(strategy_account).functions.executeRebalancePosition(
+                market=market
+            ),
+            options=send_options,
+        )
+    
+    def releverage_position(
+            self,
+            strategy_account,
+            market,
+            size_delta_usd,
+            send_options=None
+    ):
+        '''
+        Releverage a position for a strategy account.
+
+        :param strategy_account: required
+        :type strategy_account: address
+
+        :param market: required
+        :type market: address
+
+        :param size_delta_usd: required
+        :type size_delta_usd: integer
+
+        :param send_options: optional
+        :type send_options: sendOptions
+
+        :returns: None
+
+        :raise: TransactionReverted
+        ''' 
+        return self.send_transaction(
+            method=self.get_gmxfrf_strategy_account(strategy_account).functions.executeReleveragePosition(
+                market=market,
+                sizeDeltaUsd=size_delta_usd
+            ),
+            options=send_options,
+        )
+        
+    def swap_rebalance(
+            self,
+            strategy_account,
+            market,
+            callback_config,
+            send_options=None
+    ):
+        '''
+        Swap rebalance, allowing a liquidator to sell long tokens for USDC.
+
+        :param strategy_account: required
+        :type strategy_account: address
+
+        :param market: required
+        :type market: address
+
+        :param callback_config: required
+        :type callback_config: object
+
+        :param send_options: optional
+        :type send_options: sendOptions
+
+        :returns: None
+
+        :raise: TransactionReverted
+        ''' 
+        return self.send_transaction(
+            method=self.get_gmxfrf_strategy_account(strategy_account).functions.executeSwapRebalance(
+                market=market,
+                callbackConfig=callback_config
+            ),
+            options=send_options,
+        )
+    
