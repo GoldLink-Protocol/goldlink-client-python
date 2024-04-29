30552021db263c271ec79beb7f145003df4e17d8
Sam Weinberg <samweinberg@sams-mbp.lan>
2024-04-29 14:49:42 -0700

    claim script

--- examples/strategies/gmx_frf/increase_position.py
+++ examples/strategies/gmx_frf/increase_position.py
@@ -11,7 +11,6 @@ from web3 import Web3
 from dotenv import load_dotenv
 
 from goldlink import Client
-from goldlink.modules.strategies.gmx_frf.constants import MANAGER, ACCOUNT_GETTERS, CONTRACTS
 from goldlink import constants
 
 load_dotenv()
